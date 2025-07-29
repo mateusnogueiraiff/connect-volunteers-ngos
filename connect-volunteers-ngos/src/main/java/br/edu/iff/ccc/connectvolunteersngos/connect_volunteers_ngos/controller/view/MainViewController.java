88e2280b0d4af9e9e00e771992f05f4be4b06fe7
mateusnogueiraiff <mateus.nogueira@gsuite.iff.edu.br>
2025-07-28 22:18:53 -0300

    TR02-Preparacao Ambiente

---
+++ connect-volunteers-ngos/src/main/java/br/edu/iff/ccc/connectvolunteersngos/connect_volunteers_ngos/controller/view/MainViewController.java
@@ -0,0 +1,17 @@
+package br.edu.iff.ccc.connectvolunteersngos.connect_volunteers_ngos.controller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+@RequestMapping(path = "principal")
+public class MainViewController {
+    
+    @GetMapping("/")
+    public String getHomePage() {
+        return "index.html";
+    }
+    
+}
---
+++ connect-volunteers-ngos/src/test/java/br/edu/iff/ccc/connectvolunteersngos/connect_volunteers_ngos/ConnectVolunteersNgosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iff.ccc.connectvolunteersngos.connect_volunteers_ngos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectVolunteersNgosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
