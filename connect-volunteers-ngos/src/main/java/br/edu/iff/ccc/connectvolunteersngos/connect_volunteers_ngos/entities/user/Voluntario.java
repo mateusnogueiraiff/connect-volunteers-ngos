5316f7677ef5faff1596819a96d9ab1020ec409a
mateusnogueiraiff <mateus.nogueira@gsuite.iff.edu.br>
2025-08-26 03:02:16 -0300

    Atríbutos dataNasc e idade adicionados

--- connect-volunteers-ngos/src/main/java/br/edu/iff/ccc/connectvolunteersngos/connect_volunteers_ngos/entities/user/Voluntario.java
+++ connect-volunteers-ngos/src/main/java/br/edu/iff/ccc/connectvolunteersngos/connect_volunteers_ngos/entities/user/Voluntario.java
@@ -1,15 +1,38 @@
 package br.edu.iff.ccc.connectvolunteersngos.connect_volunteers_ngos.entities.user;
 
+import java.time.LocalDate;
+
+import org.springframework.cglib.core.Local;
+
 import jakarta.persistence.Entity;
 
 @Entity
 public class Voluntario extends Usuario {
 
+    private LocalDate dataNasc;
+    private int idade;
+
     public Voluntario(){
 
     }
 
-    public Voluntario(Long id, String nome, String email, String senha, String telefone, String funcao) {
+    public Voluntario(Long id, String nome, String email, String senha, String telefone, String funcao, LocalDate dataNasc, int idade) {
         super(id, nome, email, senha, telefone, funcao);
+        this.dataNasc = dataNasc;
+        this.idade = idade;
+    }
+
+    public LocalDate getDataNasc() {
+        return dataNasc;
+    }
+    public void setDataNasc(LocalDate dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
     }
 }
