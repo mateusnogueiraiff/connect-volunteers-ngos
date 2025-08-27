49e9ce9ebe8d87d3ecccd5ad72574ee916feb853
mateusnogueiraiff <mateus.nogueira@gsuite.iff.edu.br>
2025-08-26 23:36:13 -0300

    delete de arqs desnecessários e modificações pontuais

--- connect-volunteers-ngos/src/main/java/br/edu/iff/ccc/connectvolunteersngos/connect_volunteers_ngos/controller/view/login_cadastro/LoginControllerView.java
+++ connect-volunteers-ngos/src/main/java/br/edu/iff/ccc/connectvolunteersngos/connect_volunteers_ngos/controller/view/login_cadastro/LoginControllerView.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class LoginControllerView {
 
     @GetMapping("/login")
-    public String mexibirLogin() {
+    public String exibirLogin() {
         return "login-cadastro/login";
     }
 
--- connect-volunteers-ngos/src/main/java/br/edu/iff/ccc/connectvolunteersngos/connect_volunteers_ngos/entities/user/Administrador.java
+++ connect-volunteers-ngos/src/main/java/br/edu/iff/ccc/connectvolunteersngos/connect_volunteers_ngos/entities/user/Administrador.java
@@ -1,13 +1,9 @@
 package br.edu.iff.ccc.connectvolunteersngos.connect_volunteers_ngos.entities.user;
 
 import jakarta.persistence.Entity;
-import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class Administrador extends Usuario {
-
-    @NotNull
-    private int idAdministrador;
 
     public Administrador(){
 
@@ -15,14 +11,6 @@ public class Administrador extends Usuario {
 
     public Administrador(int idAdministrador, Long id, String nome, String email, String senha, String telefone, String funcao) {
         super(id, nome, email, senha, telefone, funcao);
-        this.idAdministrador = idAdministrador;
-    }
-
-    public int getIdAdminstrador() {
-        return idAdministrador;
-    }
-    public void setIdAdminstrador(int idAdministrador) {
-        this.idAdministrador = idAdministrador;
     }
 }
 
