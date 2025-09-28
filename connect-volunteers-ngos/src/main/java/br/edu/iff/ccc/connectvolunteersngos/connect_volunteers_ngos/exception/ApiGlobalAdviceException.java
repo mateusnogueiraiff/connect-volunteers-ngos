ba27495711f6715e16e7ddfcf72d23bba49f25a8
mateusnogueiraiff <mateus.nogueira@gsuite.iff.edu.br>
2025-09-27 21:14:26 -0300

    Implementação ProblemDetail

--- connect-volunteers-ngos/src/main/java/br/edu/iff/ccc/connectvolunteersngos/connect_volunteers_ngos/exception/ApiGlobalAdviceException.java
+++ connect-volunteers-ngos/src/main/java/br/edu/iff/ccc/connectvolunteersngos/connect_volunteers_ngos/exception/ApiGlobalAdviceException.java
@@ -1,23 +1,30 @@
 package br.edu.iff.ccc.connectvolunteersngos.connect_volunteers_ngos.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice // captura exceptions globalmente
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@RestControllerAdvice
 public class ApiGlobalAdviceException {
 
-    /* se um endpoint REST lançar VoluntarioNaoEncontradoException, a API devolve:
-    
-        HTTP 404 Not Found
-        "Voluntário com id X não encontrado."
-
-    */
     @ExceptionHandler(VoluntarioNaoEncontradoException.class)
-    public ResponseEntity<String> handleVoluntarioNaoEncontrado(VoluntarioNaoEncontradoException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
-    }
+    public ProblemDetail handleVoluntarioNaoEncontrado(HttpServletRequest req, VoluntarioNaoEncontradoException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
 
-    // add outros tratamentos de erro dps também
+        problemDetail.setTitle("Voluntário não encontrado");
+        problemDetail.setProperty("url", req.getRequestURL().toString());
+        problemDetail.setProperty("timestamp", LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()).toString());
+        problemDetail.setProperty("status", HttpStatus.NOT_FOUND.value());
+        problemDetail.setProperty("message", ex.getMessage());
+        problemDetail.setProperty("exception", ex.getClass().getName());
+        problemDetail.setProperty("path", req.getRequestURI());
+
+        return problemDetail;
+    }
 }
