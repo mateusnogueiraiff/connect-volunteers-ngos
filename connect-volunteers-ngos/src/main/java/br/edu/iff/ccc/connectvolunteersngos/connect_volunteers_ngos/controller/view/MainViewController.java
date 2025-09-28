8b92781ea2b9a571760077788dd226edcd07c50c
mateusnogueiraiff <mateus.nogueira@gsuite.iff.edu.br>
2025-09-28 02:23:45 -0300

    Teste para retornar template personalizada (erro 500)

--- connect-volunteers-ngos/src/main/java/br/edu/iff/ccc/connectvolunteersngos/connect_volunteers_ngos/controller/view/MainViewController.java
+++ connect-volunteers-ngos/src/main/java/br/edu/iff/ccc/connectvolunteersngos/connect_volunteers_ngos/controller/view/MainViewController.java
@@ -4,14 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 @RequestMapping(path = "principal")
 public class MainViewController {
     
     @GetMapping()
     public String getHomePage() {
+        if (true) { //condição para forçar o erro
+            throw new NullPointerException("Simulando um erro interno!"); //teste da template erro 500
+        }
         return "index.html";
     }
-    
 }
