cc17d2284ec6800f4bc94c156b5bd0c484cc797d
mateusnogueiraiff <mateus.nogueira@gsuite.iff.edu.br>
2025-09-29 17:28:29 -0300

    Alteração do package

---
+++ connect-volunteers-ngos/src/main/java/br/edu/iff/ccc/connectvolunteersngos/connect_volunteers_ngos/exception/users/VoluntarioNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package br.edu.iff.ccc.connectvolunteersngos.connect_volunteers_ngos.exception.users;
+
+public class VoluntarioNaoEncontradoException extends RuntimeException {
+    public VoluntarioNaoEncontradoException(String message) {
+        super(message);
+    }
+}
