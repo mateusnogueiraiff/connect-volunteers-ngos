c302a2092c0aaf535fffe1e868c543c00ba08150
mateusnogueiraiff <mateus.nogueira@gsuite.iff.edu.br>
2025-09-29 17:28:07 -0300

    Implementado exception para API e para View

--- connect-volunteers-ngos/src/main/java/br/edu/iff/ccc/connectvolunteersngos/connect_volunteers_ngos/exception/ApiGlobalAdviceException.java
+++ connect-volunteers-ngos/src/main/java/br/edu/iff/ccc/connectvolunteersngos/connect_volunteers_ngos/exception/ApiGlobalAdviceException.java
@@ -6,6 +6,10 @@ import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import br.edu.iff.ccc.connectvolunteersngos.connect_volunteers_ngos.exception.users.AdministradorNaoEncontradoException;
+import br.edu.iff.ccc.connectvolunteersngos.connect_volunteers_ngos.exception.users.RepresentanteNaoEncontradoException;
+import br.edu.iff.ccc.connectvolunteersngos.connect_volunteers_ngos.exception.users.VoluntarioNaoEncontradoException;
+
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -13,11 +17,60 @@ import java.time.ZoneId;
 @RestControllerAdvice
 public class ApiGlobalAdviceException {
 
+    //Voluntario
     @ExceptionHandler(VoluntarioNaoEncontradoException.class)
     public ProblemDetail handleVoluntarioNaoEncontrado(HttpServletRequest req, VoluntarioNaoEncontradoException ex) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
 
         problemDetail.setTitle("Voluntário não encontrado");
+        problemDetail.setProperty("url", req.getRequestURL().toString());
+        problemDetail.setProperty("timestamp", LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()).toString());
+        problemDetail.setProperty("status", HttpStatus.NOT_FOUND.value());
+        problemDetail.setProperty("message", ex.getMessage());
+        problemDetail.setProperty("exception", ex.getClass().getName());
+        problemDetail.setProperty("path", req.getRequestURI());
+
+        return problemDetail;
+    }
+
+    //Representante
+    @ExceptionHandler(RepresentanteNaoEncontradoException.class)
+    public ProblemDetail handleRepresentanteNaoEncontrado(HttpServletRequest req, RepresentanteNaoEncontradoException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+
+        problemDetail.setTitle("Representante não encontrado");
+        problemDetail.setProperty("url", req.getRequestURL().toString());
+        problemDetail.setProperty("timestamp", LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()).toString());
+        problemDetail.setProperty("status", HttpStatus.NOT_FOUND.value());
+        problemDetail.setProperty("message", ex.getMessage());
+        problemDetail.setProperty("exception", ex.getClass().getName());
+        problemDetail.setProperty("path", req.getRequestURI());
+
+        return problemDetail;
+    }
+
+    //ONG
+    @ExceptionHandler(OngNaoEncontradaException.class)
+    public ProblemDetail handleOngNaoEncontrada(HttpServletRequest req, OngNaoEncontradaException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+
+        problemDetail.setTitle("ONG não encontrada");
+        problemDetail.setProperty("url", req.getRequestURL().toString());
+        problemDetail.setProperty("timestamp", LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()).toString());
+        problemDetail.setProperty("status", HttpStatus.NOT_FOUND.value());
+        problemDetail.setProperty("message", ex.getMessage());
+        problemDetail.setProperty("exception", ex.getClass().getName());
+        problemDetail.setProperty("path", req.getRequestURI());
+
+        return problemDetail;
+    }
+
+    //Administrador
+    @ExceptionHandler(AdministradorNaoEncontradoException.class)
+    public ProblemDetail handleAdministradorNaoEncontrado(HttpServletRequest req, AdministradorNaoEncontradoException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+
+        problemDetail.setTitle("Administrador não encontrado");
         problemDetail.setProperty("url", req.getRequestURL().toString());
         problemDetail.setProperty("timestamp", LocalDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()).toString());
         problemDetail.setProperty("status", HttpStatus.NOT_FOUND.value());
--- connect-volunteers-ngos/src/main/java/br/edu/iff/ccc/connectvolunteersngos/connect_volunteers_ngos/exception/ViewGlobalAdviceException.java
+++ connect-volunteers-ngos/src/main/java/br/edu/iff/ccc/connectvolunteersngos/connect_volunteers_ngos/exception/ViewGlobalAdviceException.java
@@ -4,12 +4,34 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import br.edu.iff.ccc.connectvolunteersngos.connect_volunteers_ngos.exception.users.AdministradorNaoEncontradoException;
+import br.edu.iff.ccc.connectvolunteersngos.connect_volunteers_ngos.exception.users.RepresentanteNaoEncontradoException;
+import br.edu.iff.ccc.connectvolunteersngos.connect_volunteers_ngos.exception.users.VoluntarioNaoEncontradoException;
+
 @ControllerAdvice
 public class ViewGlobalAdviceException {
 
     // devolve uma pag de erro
     @ExceptionHandler(VoluntarioNaoEncontradoException.class)
     public String handleVoluntarioNaoEncontrado(VoluntarioNaoEncontradoException ex, Model model) {
+        model.addAttribute("errorMessage", ex.getMessage());
+        return "error";
+    }
+
+    @ExceptionHandler(RepresentanteNaoEncontradoException.class)
+    public String handleRepresentanteNaoEncontrado(RepresentanteNaoEncontradoException ex, Model model) {
+        model.addAttribute("errorMessage", ex.getMessage());
+        return "error";
+    }
+
+    @ExceptionHandler(OngNaoEncontradaException.class)
+    public String handleOngNaoEncontrada(OngNaoEncontradaException ex, Model model) {
+        model.addAttribute("errorMessage", ex.getMessage());
+        return "error";
+    }
+
+    @ExceptionHandler(AdministradorNaoEncontradoException.class)
+    public String handleAdministradorNaoEncontrado(AdministradorNaoEncontradoException ex, Model model) {
         model.addAttribute("errorMessage", ex.getMessage());
         return "error";
     }
